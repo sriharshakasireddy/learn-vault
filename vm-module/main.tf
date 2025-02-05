6720ad21ed3145812ef16a92ada0463dae9efcf6
iray3 <kasireddy.sriharsha@gmail.com>
2025-02-05 19:53:38 +0530

    dd

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -12,6 +12,7 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   allocation_method   = "Dynamic"
+  sku = "Basic"
 
   tags = {
     component = "${var.component}-"
