84b8664766cb526003750438bdba121a2093760d
iray3 <kasireddy.sriharsha@gmail.com>
2025-02-05 19:34:09 +0530

    dd

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -87,7 +87,7 @@ resource "azurerm_virtual_machine" "main" {
   location              = data.azurerm_resource_group.main.location
   resource_group_name   = data.azurerm_resource_group.main.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.vm_size
+  vm_size               = "Standard_B2s"
 
 
 
