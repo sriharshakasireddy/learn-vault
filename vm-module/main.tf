4cd8721c9eb7add46eb6f4c850a9dbca2ca08a51
iray3 <kasireddy.sriharsha@gmail.com>
2025-02-06 16:15:56 +0530

    dd

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -74,16 +74,22 @@ resource "azurerm_network_interface_security_group_association" "example" {
 
 
 
-resource "azurerm_dns_a_record" "main" {
-  name                = var.component
+resource "azurerm_dns_a_record" "private" {
+  name                = "${var.component}-internal"
   zone_name           = "devopsazurepractice.online"
   resource_group_name = data.azurerm_resource_group.main.name
   ttl                 = 10
   records             = [azurerm_network_interface.main.private_ip_address]
 }
-
+resource "azurerm_dns_a_record" "public" {
+  name                = var.component
+  zone_name           = "devopsazurepractice.online"
+  resource_group_name = data.azurerm_resource_group.main.name
+  ttl                 = 10
+  records             = [azurerm_public_ip.main.ip_address]
+}
 resource "azurerm_virtual_machine" "main" {
-  depends_on = [azurerm_network_interface_security_group_association.example, azurerm_dns_a_record.main]
+  depends_on = [azurerm_network_interface_security_group_association.example, azurerm_dns_a_record.private]
   name                  = var.component
   location              = data.azurerm_resource_group.main.location
   resource_group_name   = data.azurerm_resource_group.main.name
