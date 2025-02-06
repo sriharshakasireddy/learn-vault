22a8babe19c2429b4a7de1f3606faf45e8b600f8
iray3 <kasireddy.sriharsha@gmail.com>
2025-02-06 16:44:22 +0530

    dd

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_security_group" "main" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_port_range          = var.port
+    source_port_range          = "*"
     destination_port_range     = var.port
     source_address_prefix      = "*"
     destination_address_prefix = "*"
